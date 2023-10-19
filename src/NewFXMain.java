cbbcfd82947f415d4fb0ab56a33263b13f25cfbb
Wajid Hussain <52674797+wajidengg@users.noreply.github.com>
2023-10-19 02:15:44 -0400

    Modified code for new basic form

--- src/NewFXMain.java
+++ src/NewFXMain.java
@@ -3,11 +3,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
  */
 
+import com.sun.tools.javac.Main;
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -17,9 +22,12 @@ import javafx.stage.Stage;
  */
 public class NewFXMain extends Application {
     
+    private static Stage stg;
+    // private BorderPane mainLayout;
+    
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
+    public void start(Stage primaryStage) throws IOException {
+        /* Button btn = new Button();
         btn.setText("Say 'Hello World'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
             
@@ -37,8 +45,33 @@ public class NewFXMain extends Application {
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
         primaryStage.show();
+        */
+        stg = primaryStage;
+        primaryStage.setResizable(false);
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLmain.fxml"));
+        primaryStage.setTitle("Basic Form");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+        // this.primaryStage = primaryStage;
+        // this.primaryStage.setTitle("Client Form");
+        // showMainView();
     }
-
+    
+    public void changeScene(String fxml) throws IOException {
+        Parent pane = FXMLLoader.load(getClass().getResource(fxml));
+        stg.getScene().setRoot(pane);
+    
+    }
+/*
+    private void showMainView() throws IOException{
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("MainView.fxml"));
+        mainLayout = loader.load();
+        Scene scene = new Scene(mainLayout);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        
+    }*/
     /**
      * @param args the command line arguments
      */
